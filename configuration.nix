1426d23cf402483f1233ce7b2bafa314c1257197
Mathieu Boespflug <m@tweag.io>
2019-03-03 15:57:10 +0100

    Switch to latest kernel. 4.14 is getting old.

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
 
   boot = {
     cleanTmpDir = true;
+    kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     supportedFilesystems = [ "exfat" "zfs" ];
