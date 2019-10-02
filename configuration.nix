3c24eaabedccaad090019438a8e9fa419ca253d9
Mathieu Boespflug <m@tweag.io>
2019-10-02 14:42:07 +0200

    Define a systemd service that suspends if the battery is too low.

--- configuration.nix
+++ configuration.nix
@@ -85,6 +85,24 @@
     longitude = "2.3";
   };
 
+  systemd.timers.suspend-on-low-battery = {
+    wantedBy = [ "multi-user.target" ];
+    timerConfig = {
+      OnUnitActiveSec = "120";
+    };
+  };
+  systemd.services.suspend-on-low-battery =
+    let battery-level-sufficient =
+          pkgs.writeShellScriptBin "battery-level-sufficient" ''
+            test "$(cat /sys/class/power_supply/BAT1/status)" != Discharging \
+              || test "$(cat /sys/class/power_supply/BAT1/capacity)" -ge 10
+            '';
+    in
+    { serviceConfig = { Type = "oneshot"; };
+      onFailure = [ "suspend.target" ];
+      script = "${battery-level-sufficient}/bin/battery-level-sufficient";
+    };
+
   virtualisation.docker.enable = true;
   virtualisation.docker.storageDriver = "zfs";
 
