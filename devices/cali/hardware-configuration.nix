209f3e59c6f6887737157cda351dc0e66208dd4d
Mathieu Boespflug <m@tweag.io>
2023-10-11 17:39:08 +0300

    Remove hardware.video.hidpi.enable.
    It's deprecated and has no effect.

--- devices/cali/hardware-configuration.nix
+++ devices/cali/hardware-configuration.nix
@@ -47,5 +47,4 @@
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
