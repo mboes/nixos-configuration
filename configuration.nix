bf41bd3e3f3d80b20ce70f56c1be75ce5da4cd84
Mathieu Boespflug <m@tweag.io>
2025-02-19 13:46:08 +0200

    Drop Systemd low battery logic
    It never seemed to work

--- configuration.nix
+++ configuration.nix
@@ -97,24 +97,6 @@ let secrets = import ./secrets.nix; in
     upower.enable = true;
   };
 
-  systemd.timers.suspend-on-low-battery = {
-    wantedBy = [ "multi-user.target" ];
-    timerConfig = {
-      OnUnitActiveSec = "120";
-    };
-  };
-  systemd.services.suspend-on-low-battery =
-    let battery-level-sufficient =
-          pkgs.writeShellScriptBin "battery-level-sufficient" ''
-            test "$(cat /sys/class/power_supply/BAT1/status)" != Discharging \
-              || test "$(cat /sys/class/power_supply/BAT1/capacity)" -ge 10
-            '';
-    in
-    { serviceConfig = { Type = "oneshot"; };
-      onFailure = [ "suspend.target" ];
-      script = "${battery-level-sufficient}/bin/battery-level-sufficient";
-    };
-
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerCompat = true;
   virtualisation.podman.dockerSocket.enable = true;
