c5540f243e54e0542c91aba66891e1cadc9d6c91
Mathieu Boespflug <m@boespfl.ug>
2025-09-16 11:35:35 +0300

    Don't use networking.useDHCP

--- devices/cali/hardware-configuration.nix
+++ devices/cali/hardware-configuration.nix
@@ -37,11 +37,6 @@
 
   swapDevices = [ { label = "swap"; } ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
   networking.hostName = "cali";
   networking.hostId = "5c17e9f9";
 
