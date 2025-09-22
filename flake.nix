1b25008cac35c6afbcd13e9dc8b38bb14359361a
Mathieu Boespflug <m@boespfl.ug>
2025-09-22 18:18:23 +0300

    Factor common parts of configuring each device

--- flake.nix
+++ flake.nix
@@ -5,20 +5,18 @@
     # This repository is private.
     secrets.url = "git+ssh://git@github.com/mboes/nixos-configuration-secrets";
   };
-  outputs = { self, nixpkgs, secrets }: {
-    nixosConfigurations.quito = nixpkgs.lib.nixosSystem {
-      modules = [
-        secrets.nixosModules.default
-        ./configuration.nix
-        ./devices/quito/hardware-configuration.nix
-      ];
+  outputs = { self, nixpkgs, secrets, ... }:
+    let myModules = deviceConfig: [
+          secrets.nixosModules.default
+          ./configuration.nix
+          deviceConfig
+        ];
+    in {
+      nixosConfigurations.quito = nixpkgs.lib.nixosSystem {
+        modules = myModules ./devices/quito/hardware-configuration.nix;
+      };
+      nixosConfigurations.cali = nixpkgs.lib.nixosSystem {
+        modules = myModules ./devices/cali/hardware-configuration.nix;
+      };
     };
-    nixosConfigurations.cali = nixpkgs.lib.nixosSystem {
-      modules = [
-        secrets.nixosModules.default
-        ./configuration.nix
-        ./devices/cali/hardware-configuration.nix
-      ];
-    };
-  };
 }
